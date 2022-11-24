df042bb170f0e96623a7dc0f28944e963b719b70
Holajuwon <36427779+Holajuwon@users.noreply.github.com>
2022-11-24 03:57:02 +0100

    fix

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
 
 app.get('/', (req, res, next) => {
     return res.status(200).json({
